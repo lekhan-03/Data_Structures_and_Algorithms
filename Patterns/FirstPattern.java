514db266723870c0426901f18f909b4d8f6c86bb
Lekhan <lekhandayananda@gmail.com>
2026-07-01 09:08:34 +0530

    Learning Pattern One

---
+++ Patterns/FirstPattern.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class FirstPattern{
+    static void printPatternOne(int n,int m){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the Number of rows");
+        int a= in.nextInt();
+        System.out.println("Enter the Number of columns");
+        int b= in.nextInt();
+        printPatternOne(a, b);
+    }
+}
